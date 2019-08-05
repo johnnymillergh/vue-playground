910c38e6f373bc0d9efc982a045892998da3fd25
Johnny Miller (锺俊) <johnnysviva@outlook.com>
2019-08-05 15:43:50 +0800

    refactor($Router): 多级路由

---
+++ src/layout/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Menu</h1>
+    <router-link to="/parent/app1">App1</router-link>
+    <router-link to="/parent/app2">App2</router-link>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,31 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Layout from '../layout'
+import Login from '../login'
 import App1 from '../App1.vue'
 import App2 from '../App2.vue'
 
 Vue.use(Router)
 
 export const constantRouterMap = [
+  {
+    path: '/parent',
+    component: Layout,
+    children: [
+      {
+        path: 'app1',
+        component: App1
+      },
+      {
+        path: 'app2',
+        component: App2
+      }
+    ]
+  },
+  {
+    path: '/login',
+    component: Login
+  },
   {
     path: '/app1',
     component: App1
