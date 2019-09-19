07a42406682bac0b49586c86b4bbb4012f6f4c21
Johnny Miller (锺俊) <johnnysviva@outlook.com>
2019-09-19 08:54:49 +0800

    feat($Promise): 模拟异步请求

--- src/views/PromiseDemo.vue
+++ src/views/PromiseDemo.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <h3>Hello, Promise Demo</h3>
+    <el-button @click="onHandlePromise">点我触发Promise</el-button>
     <ul>
       <li v-for="(item,index) in list" :key="index">{{ item }}</li>
     </ul>
@@ -18,6 +19,7 @@ export default {
   methods: {
     mockRequest1 () {
       return new Promise(resolve => {
+        console.log('发送第1个请求')
         setTimeout(() => {
           this.list.push('request1')
           resolve()
@@ -27,16 +29,25 @@ export default {
     mockRequest2 () {
       return new Promise(resolve => {
         setTimeout(() => {
+          console.log('发送第2个请求')
           this.list.push('request2')
           resolve()
         }, 1000)
       })
+    },
+    onHandlePromise () {
+      return new Promise((resolve, reject) => {
+        reject(new Error('发生错误！'))
+      })
+    },
+    init () {
+      this.mockRequest1()
+      this.mockRequest2()
+      this.list.push('3')
     }
   },
   mounted () {
-    this.mockRequest1()
-    this.mockRequest2()
-    this.list.push('3')
+    this.init()
   }
 }
 </script>
